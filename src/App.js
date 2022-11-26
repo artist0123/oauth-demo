f41ba99e6266344d16c0d7058c4b1017137eb561
artist0123 <napatart1111@gmail.com>
2022-11-27 03:36:53 +0700

    remove isSignedIn to fix OAuth 400 error

--- src/App.js
+++ src/App.js
@@ -86,7 +86,6 @@ function App() {
               onSuccess={onSuccess}
               onFailure={onFailure}
               cookiePolicy={"single_host_origin"}
-              isSignedIn={true}
             />
           </div>
         ) : (
